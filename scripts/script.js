be8e08c0b346f14e86e96c11477ae7d4ae3de16d
joy <narumoljoy@gmail.com>
2021-07-03 11:52:35 +0100

    Have friut align in the middle row

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//const prompt = require('prompt-sync')({sigint: true});
-
 const distinctFruits=[] 
 
 distinctFruits.push({name:'apple',toProbability:.23,prize:2,symbol:'🍎'})       //23%
@@ -17,21 +15,23 @@ const numReels=3
 const reelLength=10
 const reelDivs=document.getElementsByClassName("reel")
 let fruitHeight = 0 //This is approximate. We will work it out correctly with code.
-let spin = [false,false,false];
+let spin = [false,false,false]; //boolean whether to spin a reel
 
 for (let i=0;i<numReels;i++){
     let reel = buildReel(distinctFruits,reelLength) //build reel
-    reels.push(reel) //push reel in array go through every f to creat html
+    reels.push(reel) 
+    //push reel in array go through every f to create html
     reel.forEach(f => { 
         reelDivs[i].innerHTML += distinctFruits[f].symbol
     }) 
-    fruitHeight = Math.floor(reelDivs[0].scrollHeight/reelLength)
-    // Duplicating friuts so that we can scroll to the very end.
+    fruitHeight = Math.floor(reelDivs[0].scrollHeight/reelLength) //calculate fruit height in pixels = 63 pixels
+    // Repeat the forEach again to duplicate friuts so that we can scroll to the very end. We are doubling the reel lenth
     reel.forEach(f => {  
         reelDivs[i].innerHTML += distinctFruits[f].symbol
     }) 
 }
 
+//Function to pick random fruits for build reel
 function buildReel(fruits,length){
     //accepts an array of distinct fruits 
     //returns an array of length elements of random fruit indices
@@ -44,9 +44,9 @@ function buildReel(fruits,length){
 }
 
 let moneyObj = document.querySelector('#money')
-let money = document.querySelector('#money').textContent
-//let started=process.hrtime() //returns a [seconds][nanoseconds] tuple
-let costPerSpin = 20
+let money = document.querySelector('#money').textContent //get money from id 'money' in index.html
+
+let costPerSpin = 1
 let target = []
 let reelf = [0,0,0]
 
@@ -54,18 +54,12 @@ animate () // start the reel in motion
 
 function spinReels(){
         if (money < costPerSpin) {
-            alert("You are out of money - gambling is a foools game")
+            alert("You are out of money - gambling is a foools game") //alert if run out of money
         }
         else {
-        money = money - costPerSpin //Pay £1 to play
+        money = money - costPerSpin //Pay costPerSpin to play
         console.log('You have £' + money )
         moneyObj.textContent = money //update money on page
-        // prompt('Press enter to spin the wheels')  
-
-        //console.log(fruits[1]);
-
-        //scroll()
-        //setTimeout(scroll,10) //to execute a function after waiting for the specified time interval
 
         spin = [true, true, true]
         //target = where a reel stops
@@ -81,80 +75,39 @@ function spinReels(){
         console.log(win[0])
         console.log(win[1])
         console.log(win[2])
-        //if (win[0].name = "apple") {
+        console.log(win[0].name, win[1].name, win[2].name)
+        //if all reels 1- 3 get to same fruit, add prize money to money
         if (checkReelsMatch(win[0].name,win[1].name,win[2].name)){
             money += win[0].prize
         }
         }
-        // let p1 = Math.floor(Math.random() * reelLength) 
-        // let p2 = Math.floor(Math.random() * reelLength)
-        // let p3 = Math.floor(Math.random() * reelLength)
-
-        // console.log(reel1[p1] + ' ' + reel2[p2] + ' ' + reel3[p3])
-
-        // if (checkReelsMatch(p1,p2,p3)){
-
-        //     console.log ('You win :o)')
-        //     if (reel1[p1]=='Apple'){
-        //         money = money + 2
-        //         console.log ('£2')
-        //     }
-        //     else if (reel1[p1]=='Banana') {
-        //         money = money + 3
-        //         console.log ('£3')
-        //     }
-        //     else if (reel1[p1]=='Cherry') {
-        //         money = money + 5
-        //         console.log ('£5')
-        //     }
-        //     else if (reel1[p1]=='Diamond') {
-        //         money = money + 20
-        //         console.log ('£20')
-        //     }      
-        // }
-        // else{
-        //     console.log('You lose :o(')
-        // }
 }
-
-
-//let took=process.hrtime(started)
-//console.log (took[0] + "seconds and " + took[1] /1e6 + "milliseconds")
-
+// check if all 3 reel gets the same fruit
 function checkReelsMatch(p1,p2,p3){
 
     if (check3theSame(p1,p2,p3)){
         return true
     }
-
 }
-
+//check a = b && b =c
 function check3theSame(a,b,c){
 
     if(a==b && b==c){
         return true
     }
 }
-
-
-
+// start the reel in motion
 function animate() {
     let reels = document.getElementsByClassName("reel")
-    // reelf.forEach (f => {
-    //     f+= 0.1
-
-    // })
-    // reels.forEach (r =>{ // push reel variable
-    //     r.scrollTop = reelf * fruitHeight /2
-    // })
     // i = number of Reels 
     for (let i = 0; i < numReels; i++) {
         //check if the reel spinning, spinning parameter
         //reelf = fruit number
         if(spin[i]) { 
             reelf[i] += 0.1
-            reels[i].scrollTop = reelf[i] * fruitHeight / 2
-            if (reelf[i] > reelLength) { //if reel frame > reel length set it to 0 and keep rolling
+            reels[i].scrollTop = (reelf[i]-1) * fruitHeight / 2 //fruitHeight is divided by 2 as the reel is twice as long. The reel is twice as long as we duplicate friuts so that we can scroll to the very end. 
+            //Use (reelf[i]-1) to offset to the middle row. Otherwise it will be the top row.
+            if ((reelf[i]) > reelLength) { //if reel frame > reel length set it to 0 and keep rolling
                 reelf[i] = 0; 
             }    
         }
@@ -163,13 +116,9 @@ function animate() {
         //if target = 0 then set (target -1) = reel length -1
         if ((Math.floor(reelf[i]) === target[i]) && (i == 0 || spin[i-1] == false)) {
             //stop reel
-            //reels[i].scrollTop = target[i] * fruitHeight /2
             spin[i] = false; //true = wheel is spinning, false = reel is stop.
         }
     }
-
-   // console.log(reel1f)
    requestAnimationFrame (animate)
-
 }
 
